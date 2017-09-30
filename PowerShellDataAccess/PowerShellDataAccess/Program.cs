24ec27f948021d02fd2a848cb5fce59863681d08
Keith LeValley <keith.1985.kl@gmail.com>
2017-09-30 13:50:02 -0400

    Added Cust DB and started Program

---
+++ PowerShellDataAccess/PowerShellDataAccess/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data;
+using System.IO;
+
+namespace PowerShellDataAccess
+{
+
+    class Program
+    {
+        public string ConnectionString { get; set; }
+        public Dictionary<string, string> DataBases { get; private set; }
+
+        Program()
+        {
+            var DBFiles = Directory.GetFiles("DB");
+            FileInfo fileInfo;
+
+            foreach (var DBFile in DBFiles)
+            {
+                fileInfo = new FileInfo(DBFile);
+
+                DataBases.Add(fileInfo.Name ,fileInfo.FullName);
+            }
+        }     
+    }
+}
